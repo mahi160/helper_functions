6490ab470437f37beb1fdd744f6c1105446a4e5e
Salauddin Omar Sifat <50174250+mahi160@users.noreply.github.com>
2020-09-27 23:41:30 +0600

    Update numberInputOnly.ts
    keyup to keydown

--- numberInputOnly.ts
+++ numberInputOnly.ts
@@ -1,33 +1,33 @@
-/** This function will only return number for a text input at keyup. 
-* Also this supports Ctrl, A, S, X for shortcuts. 
-* Use decimal for .(dot) input.
-*/
-export const validation = (keyupEvent, isDecimal: boolean) => {
+/** This function will only return number for a text input at keyup.
+ * Also this supports Ctrl, A, S, X for shortcuts.
+ * Use decimal for .(dot) input.
+ */
+export const validation = (keydownEvent, isDecimal: boolean) => {
   if (isDecimal) {
     return (
-      (keyupEvent.keyCode >= 48 && keyupEvent.keyCode <= 57) ||
-      (keyupEvent.keyCode >= 96 && keyupEvent.keyCode <= 105) ||
-      keyupEvent.keyCode === 8 ||
-      (keyupEvent.keyCode >= 37 && keyupEvent.keyCode <= 40) ||
-      keyupEvent.keyCode === 190 ||
-      keyupEvent.keyCode === 110 ||
-      keyupEvent.keyCode === 17 ||
-      keyupEvent.keyCode === 67 ||
-      keyupEvent.keyCode === 86 ||
-      keyupEvent.key === "Meta" ||
-      keyupEvent.keyCode === 65
+      (keydownEvent.keyCode >= 48 && keydownEvent.keyCode <= 57) ||
+      (keydownEvent.keyCode >= 96 && keydownEvent.keyCode <= 105) ||
+      keydownEvent.keyCode === 8 ||
+      (keydownEvent.keyCode >= 37 && keydownEvent.keyCode <= 40) ||
+      keydownEvent.keyCode === 190 ||
+      keydownEvent.keyCode === 110 ||
+      keydownEvent.keyCode === 17 ||
+      keydownEvent.keyCode === 67 ||
+      keydownEvent.keyCode === 86 ||
+      keydownEvent.key === "Meta" ||
+      keydownEvent.keyCode === 65
     );
   } else {
     return (
-      (keyupEvent.keyCode >= 48 && keyupEvent.keyCode <= 57) ||
-      (keyupEvent.keyCode >= 96 && keyupEvent.keyCode <= 105) ||
-      keyupEvent.keyCode === 8 ||
-      (keyupEvent.keyCode >= 37 && keyupEvent.keyCode <= 40) ||
-      keyupEvent.keyCode === 17 ||
-      keyupEvent.keyCode === 67 ||
-      keyupEvent.keyCode === 86 ||
-      keyupEvent.key === "Meta" ||
-      keyupEvent.keyCode === 65
+      (keydownEvent.keyCode >= 48 && keydownEvent.keyCode <= 57) ||
+      (keydownEvent.keyCode >= 96 && keydownEvent.keyCode <= 105) ||
+      keydownEvent.keyCode === 8 ||
+      (keydownEvent.keyCode >= 37 && keydownEvent.keyCode <= 40) ||
+      keydownEvent.keyCode === 17 ||
+      keydownEvent.keyCode === 67 ||
+      keydownEvent.keyCode === 86 ||
+      keydownEvent.key === "Meta" ||
+      keydownEvent.keyCode === 65
     );
   }
 };
